3c54b3e99c276c8dcd0c44b346aed910d327a0c1
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-03-28 22:26:47 +0300

    remove in app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,6 @@ import '@/styles/globals.scss'
 import classNames from 'classnames'
 import { NextComponentType, NextPageContext } from 'next'
 import type { AppProps } from 'next/app'
-import { ReactElement, ReactNode } from 'react';
-import { NextPage } from "next";
-
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
 
 export default function App({ Component, pageProps }: MyAppPropsType) {
   const getLayout = Component.getLayout || ((page) => page)
