694120218560206e755b016ebd20b24b900717cf
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-03-29 16:39:41 +0300

    fix form default value

--- src/modules/ProjectsList/ProjectsList.tsx
+++ src/modules/ProjectsList/ProjectsList.tsx
@@ -1,169 +1,296 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 import { useForm, Controller } from "react-hook-form";
 
-import { Typography, Input, Radio, Select, Button, Modal, Space } from 'antd';
-import { SearchOutlined, CheckOutlined } from '@ant-design/icons';
+import { Typography, Input, Radio, Select, Button, Modal, Space } from "antd";
+import { SearchOutlined, CheckOutlined } from "@ant-design/icons";
 
-import { PorjectsProps } from './ProjectsList.types';
+import { PorjectsProps } from "./ProjectsList.types";
 
-import s from './ProjectsList.module.scss'
+import s from "./ProjectsList.module.scss";
 
 export function ProjectsList({ ...props }: PorjectsProps) {
-    const { Title, Text } = Typography;
-    const { Search } = Input;
+	const { Title, Text } = Typography;
+	const { Search } = Input;
 
-    const onSearch = (e: any) => {
-        console.log(e)
-    }
+	const onSearch = (e: any) => {
+		console.log(e);
+	};
 
-    const options = [
-        { label: 'По дате добавления', value: 'По дате добавления' },
-        { label: 'По дате изменения', value: 'По дате изменения' },
-        { label: 'По названию', value: 'По названию' },
-    ];
+	const options = [
+		{ label: "По дате добавления", value: "По дате добавления" },
+		{ label: "По дате изменения", value: "По дате изменения" },
+		{ label: "По названию", value: "По названию" },
+	];
 
-    const [sort, setSort] = useState('По дате добавления');
+	const [sort, setSort] = useState("По дате добавления");
 
-    const onChangeSort = (value: any) => {
-        const text = value.target.value
-        console.log(text);
-        setSort(text);
-    };
+	const onChangeSort = (value: any) => {
+		const text = value.target.value;
+		console.log(text);
+		setSort(text);
+	};
 
-    const onChangeSelect = (value: any) => {
-        console.log(value)
-    }
+	const onChangeSelect = (value: any) => {
+		console.log(value);
+	};
 
-    const [localStorage, setLocalStorage] = useState([
-        {
-            id: 0,
-            title: 'Тестовый проект №1',
-            date: 'Добавлено 23.03.2023, обновлено 25.03.2023',
-        },
-        {
-            id: 1,
-            title: 'Проект 2',
-            date: 'Добавлено 23.03.2023, обновлено 25.03.2023',
-        }
-    ])
+	const [localStorage, setLocalStorage] = useState([
+		{
+			id: 0,
+			title: "Тестовый проект №1",
+			date: "Добавлено 23.03.2023, обновлено 25.03.2023",
+		},
+		{
+			id: 1,
+			title: "Проект 2",
+			date: "Добавлено 23.03.2023, обновлено 25.03.2023",
+		},
+	]);
 
-    const [editableProject, setEditableProject] = useState<number>()
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const open = () => {
-        console.log('Открыть этот проект')
-    }
+	const { control, handleSubmit, setValue } = useForm();
 
-    const edit = (id: number) => {
-        setEditableProject(id)
-        setIsModalOpen(true)
-        console.log('Изменить этот проект')
-    }
+	const open = () => {
+		console.log("Открыть этот проект");
+	};
 
-    const copy = () => {
-        console.log('Скопировать этот проект')
-    }
+	const edit = (id: number) => {
+		setValue(
+			"title",
+			() =>
+				localStorage.find((current) => {
+					return current.id === id;
+				})?.title ?? ""
+		);
+		setIsModalOpen(true);
+		console.log("Изменить этот проект");
+	};
 
-    const remove = () => {
-        console.log('Удалить этот проект')
-    }
+	const copy = () => {
+		console.log("Скопировать этот проект");
+	};
 
-    const close = (e: any) => {
-        // setIsModalOpen(false)
-        // (e) => setLocalStorage((currentLocalStorage) => (currentLocalStorage.map(item => item.id === editableProject ? {...item, title: e.target.value} : item)))
-    }
+	const remove = () => {
+		console.log("Удалить этот проект");
+	};
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+	const close = (e: any) => {
+		setIsModalOpen(false);
+	};
 
-    const { control, handleSubmit } = useForm({ defaultValues: { title: localStorage.find(current => current.id === editableProject)?.title } });
+	const onSubmit = (data: any) => {
+		console.log(data);
+	};
 
-    const onSubmit = (data: any) => {
-        console.log(data);
-    };
+	return (
+		<div className={s.wrapper}>
+			<Modal
+				title="Название"
+				open={isModalOpen}
+				footer={null}
+				onCancel={close}
+			>
+				<form className={s.input} onSubmit={handleSubmit(onSubmit)}>
+					<Space.Compact style={{ width: "100%" }}>
+						<Controller
+							name="title"
+							control={control}
+							render={({ field }) => (
+								<Input
+									{...field}
+									placeholder="Введите название"
+								/>
+							)}
+						/>
+						<Button
+							htmlType="submit"
+							type="primary"
+							icon={<CheckOutlined />}
+						></Button>
+					</Space.Compact>
+				</form>
+			</Modal>
+			<div className={s.header}>
+				<div className={s.header__search}>
+					<Title level={5} className={s.header__title}>
+						Список проектов
+					</Title>
+					<div className={s.header__input}>
+						<Search
+							key="search"
+							id="search"
+							placeholder="Поиск..."
+							onSearch={onSearch}
+							enterButton={[
+								<SearchOutlined key={"searchIcon"} />,
+								"Найти",
+							]}
+						/>
+					</div>
+				</div>
+				<div className={s.header__sort}>
+					<Select
+						defaultValue="По возрастанию"
+						className={s.header__select}
+						bordered={false}
+						options={[
+							{
+								value: "По возрастанию",
+								label: "По возрастанию",
+							},
+							{ value: "По убыванию", label: "По убыванию" },
+						]}
+					/>
+					<Radio.Group
+						options={options}
+						onChange={onChangeSort}
+						value={sort}
+						optionType="button"
+					/>
+				</div>
+			</div>
+			<div className={s.list}>
+				{localStorage.map((current) => {
+					return (
+						<div key={current.id} className={s.list__item}>
+							<svg
+								className={s.list__svg}
+								width="34"
+								height="28"
+								viewBox="0 0 34 28"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									className={s.list__svg}
+									stroke="#1890FF"
+									d="M4.03709 0.666748H11.4445L17.0001 6.00008H29.963C30.9453 6.00008 31.8874 6.37468 32.5819 7.04148C33.2765 7.70828 33.6667 8.61265 33.6667 9.55564V23.7779C33.6667 24.7209 33.2765 25.6252 32.5819 26.292C31.8874 26.9588 30.9453 27.3334 29.963 27.3334H4.03709C3.05481 27.3334 2.11276 26.9588 1.41818 26.292C0.723598 25.6252 0.333387 24.7209 0.333387 23.7779V4.2223C0.333387 3.27931 0.723598 2.37494 1.41818 1.70815C2.11276 1.04135 3.05481 0.666748 4.03709 0.666748Z"
+									fill="#1890FF"
+								/>
+							</svg>
+							<div className={s.list__info}>
+								<Title level={5}>{current.title}</Title>
+								<Text type="secondary">{current.date}</Text>
+							</div>
+							<Button onClick={open}>Открыть</Button>
+							<div className={s.menu}>
+								<svg
+									onClick={() => edit(current.id)}
+									className={s.menu__svg}
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<g
+										className={s.menu__svg}
+										clipPath="url(#clip0_179_10303)"
+									>
+										<path
+											d="M3.99927 20.0001H7.99927L18.4993 9.50006C19.0297 8.96963 19.3277 8.2502 19.3277 7.50006C19.3277 6.74991 19.0297 6.03049 18.4993 5.50006C17.9688 4.96963 17.2494 4.67163 16.4993 4.67163C15.7491 4.67163 15.0297 4.96963 14.4993 5.50006L3.99927 16.0001V20.0001Z"
+											stroke="#1890FF"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+										<path
+											d="M13.4999 6.49988L17.4999 10.4999"
+											stroke="#1890FF"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+									</g>
+								</svg>
 
-    return (
-        <div className={s.wrapper}>
-            <Modal title="Название" open={isModalOpen} footer={null} onCancel={() => setIsModalOpen(false)}>
-                <form className={s.input} onSubmit={handleSubmit(onSubmit)}>
-                    <Space.Compact style={{ width: '100%' }}>
-                        <Controller
-                            name="title"
-                            control={control}
-                            render={({ field }) => (
-                                <Input
-                                    {...field}
-                                    placeholder="Введите название"
-                                />
-                            )}
-                        />
-                        <Button htmlType='submit' type="primary" icon={<CheckOutlined />}></Button>
-                    </Space.Compact>
-                </form>
-            </Modal>
-            <div className={s.header}>
-                <div className={s.header__search}>
-                    <Title level={5} className={s.header__title}>Список проектов</Title>
-                    <div className={s.header__input}>
-                        <Search key='search' id='search' placeholder="Поиск..." onSearch={onSearch} enterButton={[<SearchOutlined />, 'Найти']} />
-                    </div>
-                </div>
-                <div className={s.header__sort}>
-                    <Select
-                        defaultValue="По возрастанию"
-                        className={s.header__select}
-                        bordered={false}
-                        options={[
-                            { value: 'По возрастанию', label: 'По возрастанию' },
-                            { value: 'По убыванию', label: 'По убыванию' },
-                        ]}
-                    />
-                    <Radio.Group options={options} onChange={onChangeSort} value={sort} optionType="button" />
-                </div>
-            </div>
-            <div className={s.list}>
-                {
-                    localStorage.map(current => {
-                        return (
-                            <div key={current.id} className={s.list__item}>
-                                <svg className={s.list__svg} width="34" height="28" viewBox="0 0 34 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path className={s.list__svg} stroke='#1890FF' d="M4.03709 0.666748H11.4445L17.0001 6.00008H29.963C30.9453 6.00008 31.8874 6.37468 32.5819 7.04148C33.2765 7.70828 33.6667 8.61265 33.6667 9.55564V23.7779C33.6667 24.7209 33.2765 25.6252 32.5819 26.292C31.8874 26.9588 30.9453 27.3334 29.963 27.3334H4.03709C3.05481 27.3334 2.11276 26.9588 1.41818 26.292C0.723598 25.6252 0.333387 24.7209 0.333387 23.7779V4.2223C0.333387 3.27931 0.723598 2.37494 1.41818 1.70815C2.11276 1.04135 3.05481 0.666748 4.03709 0.666748Z" fill="#1890FF" />
-                                </svg>
-                                <div className={s.list__info}>
-                                    <Title level={5}>{current.title}</Title>
-                                    <Text type='secondary'>{current.date}</Text>
-                                </div>
-                                <Button onClick={open}>Открыть</Button>
-                                <div className={s.menu}>
-                                    <svg onClick={() => edit(current.id)} className={s.menu__svg} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g className={s.menu__svg} clipPath="url(#clip0_179_10303)">
-                                            <path d="M3.99927 20.0001H7.99927L18.4993 9.50006C19.0297 8.96963 19.3277 8.2502 19.3277 7.50006C19.3277 6.74991 19.0297 6.03049 18.4993 5.50006C17.9688 4.96963 17.2494 4.67163 16.4993 4.67163C15.7491 4.67163 15.0297 4.96963 14.4993 5.50006L3.99927 16.0001V20.0001Z" stroke="#1890FF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                            <path d="M13.4999 6.49988L17.4999 10.4999" stroke="#1890FF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                        </g>
-                                    </svg>
+								<svg
+									onClick={copy}
+									className={s.menu__svg}
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<g
+										className={s.menu__svg}
+										clipPath="url(#clip0_179_10307)"
+									>
+										<path
+											d="M18.9999 2.99988H8.99988C7.89531 2.99988 6.99988 3.89531 6.99988 4.99988V14.9999C6.99988 16.1044 7.89531 16.9999 8.99988 16.9999H18.9999C20.1044 16.9999 20.9999 16.1044 20.9999 14.9999V4.99988C20.9999 3.89531 20.1044 2.99988 18.9999 2.99988Z"
+											stroke="#1890FF"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+										<path
+											d="M16.9998 17V19C16.9998 19.5304 16.789 20.0391 16.414 20.4142C16.0389 20.7893 15.5302 21 14.9998 21H4.99976C4.46932 21 3.96062 20.7893 3.58554 20.4142C3.21047 20.0391 2.99976 19.5304 2.99976 19V9C2.99976 8.46957 3.21047 7.96086 3.58554 7.58579C3.96062 7.21071 4.46932 7 4.99976 7H6.99976"
+											stroke="#1890FF"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+									</g>
+								</svg>
 
-                                    <svg onClick={copy} className={s.menu__svg} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g className={s.menu__svg} clipPath="url(#clip0_179_10307)">
-                                            <path d="M18.9999 2.99988H8.99988C7.89531 2.99988 6.99988 3.89531 6.99988 4.99988V14.9999C6.99988 16.1044 7.89531 16.9999 8.99988 16.9999H18.9999C20.1044 16.9999 20.9999 16.1044 20.9999 14.9999V4.99988C20.9999 3.89531 20.1044 2.99988 18.9999 2.99988Z" stroke="#1890FF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                            <path d="M16.9998 17V19C16.9998 19.5304 16.789 20.0391 16.414 20.4142C16.0389 20.7893 15.5302 21 14.9998 21H4.99976C4.46932 21 3.96062 20.7893 3.58554 20.4142C3.21047 20.0391 2.99976 19.5304 2.99976 19V9C2.99976 8.46957 3.21047 7.96086 3.58554 7.58579C3.96062 7.21071 4.46932 7 4.99976 7H6.99976" stroke="#1890FF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                        </g>
-                                    </svg>
-
-                                    <svg onClick={remove} className={s.menu__svg} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g className={s.menu__svg} clipPath="url(#clip0_179_10311)">
-                                            <path d="M3.99976 7H19.9998" stroke="#1890FF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                            <path d="M9.99976 10.9999V16.9999" stroke="#1890FF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                            <path d="M13.9998 10.9998V16.9998" stroke="#1890FF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                            <path d="M4.99976 7L5.99976 19C5.99976 19.5304 6.21047 20.0391 6.58554 20.4142C6.96062 20.7893 7.46932 21 7.99976 21H15.9998C16.5302 21 17.0389 20.7893 17.414 20.4142C17.789 20.0391 17.9998 19.5304 17.9998 19L18.9998 7" stroke="#1890FF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                            <path d="M8.99988 6.99988V3.99988C8.99988 3.73466 9.10523 3.48031 9.29277 3.29277C9.48031 3.10523 9.73466 2.99988 9.99988 2.99988H13.9999C14.2651 2.99988 14.5194 3.10523 14.707 3.29277C14.8945 3.48031 14.9999 3.73466 14.9999 3.99988V6.99988" stroke="#1890FF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                        </g>
-                                    </svg>
-
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    );
-};
+								<svg
+									onClick={remove}
+									className={s.menu__svg}
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<g
+										className={s.menu__svg}
+										clipPath="url(#clip0_179_10311)"
+									>
+										<path
+											d="M3.99976 7H19.9998"
+											stroke="#1890FF"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+										<path
+											d="M9.99976 10.9999V16.9999"
+											stroke="#1890FF"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+										<path
+											d="M13.9998 10.9998V16.9998"
+											stroke="#1890FF"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+										<path
+											d="M4.99976 7L5.99976 19C5.99976 19.5304 6.21047 20.0391 6.58554 20.4142C6.96062 20.7893 7.46932 21 7.99976 21H15.9998C16.5302 21 17.0389 20.7893 17.414 20.4142C17.789 20.0391 17.9998 19.5304 17.9998 19L18.9998 7"
+											stroke="#1890FF"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+										<path
+											d="M8.99988 6.99988V3.99988C8.99988 3.73466 9.10523 3.48031 9.29277 3.29277C9.48031 3.10523 9.73466 2.99988 9.99988 2.99988H13.9999C14.2651 2.99988 14.5194 3.10523 14.707 3.29277C14.8945 3.48031 14.9999 3.73466 14.9999 3.99988V6.99988"
+											stroke="#1890FF"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+									</g>
+								</svg>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
