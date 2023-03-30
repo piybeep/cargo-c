ed79950d8d86031c34315cda699940ff83c29027
piy-bebe <piy.bebe@gmail.com>
2023-03-30 22:28:53 +0300

    feat: добавить страницу Field

--- src/pages/visualization/index.tsx
+++ src/pages/visualization/index.tsx
@@ -1,16 +1,13 @@
 import { BaseLayout } from "@/layouts/BaseLayout";
+import { Field } from "@/modules";
 import { ReactNode } from "react";
 
 export default function Visualization() {
-	return (
-		<main>
-            Visualization
-		</main>
-	);
+  return (
+    <main>
+      <Field />
+    </main>
+  );
 }
 
-Visualization.getLayout = (page: ReactNode) => (
-	<BaseLayout>
-		{page}
-	</BaseLayout>
-);
+Visualization.getLayout = (page: ReactNode) => <BaseLayout>{page}</BaseLayout>;
