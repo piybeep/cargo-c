f52bee10fb8d8e039b070a2739ca06d17b5624d9
ivan <ivan.safonov.09@mail.ru>
2023-04-01 23:40:54 +0300

    static верстка 100%

--- src/modules/Cargo/Cargo.tsx
+++ src/modules/Cargo/Cargo.tsx
@@ -3,15 +3,26 @@ import Header from './Header/Header'
 import down from '../../../public/svg/Down.svg'
 import s from './Cargo.module.scss'
 import Group from './Group/Group'
+import replace from '../../../public/svg/replace.svg'
+import classNames from 'classnames'
+import Footer from './Footer/Footer'
 
 export const Cargo = () => {
   return (
     <>
       <Header />
       <div className={s.roll}>
-        <img src={down.src} alt='down'  />
+        <img src={down.src} alt='down' />
       </div>
-      <Group/>
+      <Group />
+      <div className={s.replace}>
+        <img src={replace.src} alt='replace' />
+      </div>
+      <Group />
+      <div className={classNames(s.roll,s.roll_mod)}>
+        <img src={down.src} alt='down' />
+      </div>
+      <Footer/>
     </>
   )
 }
