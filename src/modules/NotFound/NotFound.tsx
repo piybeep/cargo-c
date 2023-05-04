ac2421687ab067ca3c1d09797f4de39fbc257c48
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-05-04 14:11:53 +0300

    add notFoundPage

---
+++ src/modules/NotFound/NotFound.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+import { Typography } from 'antd';
+
+import { NotFoundProps } from './NotFound.types';
+import s from './NotFound.module.scss'
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+
+export function NotFound({ }: NotFoundProps) {
+    const { Text, Title } = Typography;
+    return (
+        <div className={s.wrapper}>
+            <div className={s.wrapper__info}>
+                <div className={s.wrapper__message}>
+                    <ExclamationCircleOutlined className={s.wrapper__icon} />
+                    <Title className={s.wrapper__title} level={5}>Ошибка 404. Такой страницы нет...</Title>
+                </div>
+                <Link href='/projects'>
+                    <Text underline>Перейти к проектам</Text>
+                </Link>
+            </div>
+        </div>
+    );
+};
