2b1b69c1a836e603cd8d29738280c29668eb00fd
NAVIchok <ivan.safonov.09@mail.ru>
2023-06-19 19:53:58 +0300

    fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,26 +4,26 @@ export const instance = axios.create({
   baseURL: process.env.BASE_URL,
   withCredentials: true
 })
-instance.interceptors.response.use(
-  (config) => {
-    return config
-  },
-  async (error) => {
-    const originalRequest = error.config
-    if (
-      error.response.status === 401 &&
-      error.config &&
-      !error.config._isRetry
-    ) {
-      originalRequest._isRetry = true
-      try {
-        await axios.get(process.env.BASE_URL + 'auth/sessions')
-        return instance.request(originalRequest)
-      } catch (e) {
-        localStorage.clear()
-        window.location.href = '/login'
-      }
-    }
-    throw error
-  }
-)
+// instance.interceptors.response.use(
+//   (config) => {
+//     return config
+//   },
+//   async (error) => {
+//     const originalRequest = error.config
+//     if (
+//       error.response.status === 401 &&
+//       error.config &&
+//       !error.config._isRetry
+//     ) {
+//       originalRequest._isRetry = true
+//       try {
+//         await axios.get(process.env.BASE_URL + 'auth/sessions')
+//         return instance.request(originalRequest)
+//       } catch (e) {
+//         localStorage.clear()
+//         window.location.href = '/login'
+//       }
+//     }
+//     throw error
+//   }
+// )
